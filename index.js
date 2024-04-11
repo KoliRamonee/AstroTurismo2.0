80d13429aa097311170433251d8a9b5e5b814c6f
Juanliendo22 <liendojuan070@gmail.com>
2024-04-11 13:16:13 -0300

    cambios en lugares,  y subrayado permanente para saber en que pagina estamos posicionados

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function mostrarPagina(idPagina) {
-    // Ocultar todas las páginas
+  
     var paginas = document.querySelectorAll('.pagina');
     for (var i = 0; i < paginas.length; i++) {
         paginas[i].classList.remove('activa');
     }
-    //Con esto mostramos la otra pagina
+   
     document.getElementById(idPagina).classList.add('activa');
 }
+
+
+
+  
+  
+  
